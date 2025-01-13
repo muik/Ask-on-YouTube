862fb68b9f5ca095280b2d11788f41fc9f4654ba
Ed Jeon <muikor@gmail.com>
2025-01-13 12:51:03 +0900

    refactor: reformat index.js for improved readability

--- src/contentscript/index.js
+++ src/contentscript/index.js
@@ -1,34 +1,37 @@
 "use strict";
 
 import { insertSummaryBtn } from "./youtube";
-import { detectVideoOptionClick, insertMoreOptions } from "./youtube/moreOptions";
+import {
+  detectVideoOptionClick,
+  insertMoreOptions,
+} from "./youtube/moreOptions";
 
 let oldHref = "";
 
 window.onload = async () => {
-        
-  if (window.location.hostname === "www.youtube.com") {
-        
-        if (window.location.search !== "" && window.location.search.includes("v=")) {
-      insertSummaryBtn();
-    }
-
-    const bodyList = document.querySelector("body");
-    let observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        if (oldHref !== document.location.href) {
-          oldHref = document.location.href;
-          insertSummaryBtn();
+    if (window.location.hostname === "www.youtube.com") {
+        if (
+            window.location.search !== "" &&
+            window.location.search.includes("v=")
+        ) {
+            insertSummaryBtn();
         }
-      });
-    });
-    observer.observe(bodyList, { childList: true, subtree: true });
 
-    insertMoreOptions();
+        const bodyList = document.querySelector("body");
+        let observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (oldHref !== document.location.href) {
+                    oldHref = document.location.href;
+                    insertSummaryBtn();
+                }
+            });
+        });
+        observer.observe(bodyList, { childList: true, subtree: true });
 
-    document.addEventListener("click", (event) => {
-      detectVideoOptionClick(event.target);
-    });
-  }
+        insertMoreOptions();
 
+        document.addEventListener("click", (event) => {
+            detectVideoOptionClick(event.target);
+        });
+    }
 };
