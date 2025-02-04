0d249e4d3625adb43943e7e17a4b61d98124b9f1
Ed Jeon <muikor@gmail.com>
2025-02-04 15:16:34 +0900

    chore: Add puppeteer for ui testing and update Jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,5 +1,5 @@
 export default {
     setupFiles: ["./jest.setup.js"], // Ensure Jest runs this setup before your tests
-    transform: {},
-    testEnvironment: "jsdom",
+    transform: {}, // Disable Babel transform for ES Modules
+    testTimeout: 30000, // Extend the timeout for Puppeteer tests
 };
--- src/background.js
+++ src/background.js
@@ -8,12 +8,13 @@ import {
 } from "./background/prompt.js";
 
 console.log("connected...");
-const onInstallURL = "https://glasp.co/youtube-summary";
+// const onInstallURL = "https://glasp.co/youtube-summary";
 
 // On Chrome Install
 chrome.runtime.onInstalled.addListener(function (details) {
     if (details.reason === "install") {
-        chrome.tabs.create({ url: onInstallURL });
+        // TODO replace this url
+        // chrome.tabs.create({ url: onInstallURL });
     }
 });
 
