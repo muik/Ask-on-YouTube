2d5118a50e83fa83339e8b9576118ded8b5f1267
Ed Jeon <muikor@gmail.com>
2025-02-07 20:01:15 +0900

    feat: Add question suggestions to dialog

--- src/contentscript/youtube/questionView.js
+++ src/contentscript/youtube/questionView.js
@@ -258,42 +258,43 @@ function getQuestionHtml() {
 <div id="${containerId}" role="dialog" class="style-scope ytd-popup-container ytq-dialog" style="position: fixed;">
   <ytd-unified-share-panel-renderer class="style-scope ytd-popup-container" tabindex="-1" links-only="true" can-post="">
     <div id="spinner" class="style-scope ytd-unified-share-panel-renderer" hidden="">
-    <tp-yt-paper-spinner class="style-scope ytd-unified-share-panel-renderer" aria-label="loading" aria-hidden="true"><div id="spinnerContainer" class="  style-scope tp-yt-paper-spinner">
-    <div class="spinner-layer layer-1 style-scope tp-yt-paper-spinner">
-      <div class="circle-clipper left style-scope tp-yt-paper-spinner">
-        <div class="circle style-scope tp-yt-paper-spinner"></div>
-      </div>
-      <div class="circle-clipper right style-scope tp-yt-paper-spinner">
-        <div class="circle style-scope tp-yt-paper-spinner"></div>
-      </div>
+      <tp-yt-paper-spinner class="style-scope ytd-unified-share-panel-renderer" aria-label="loading" aria-hidden="true">
+        <div id="spinnerContainer" class="  style-scope tp-yt-paper-spinner">
+          <div class="spinner-layer layer-1 style-scope tp-yt-paper-spinner">
+            <div class="circle-clipper left style-scope tp-yt-paper-spinner">
+              <div class="circle style-scope tp-yt-paper-spinner"></div>
+            </div>
+            <div class="circle-clipper right style-scope tp-yt-paper-spinner">
+              <div class="circle style-scope tp-yt-paper-spinner"></div>
+            </div>
+          </div>
+          <div class="spinner-layer layer-2 style-scope tp-yt-paper-spinner">
+            <div class="circle-clipper left style-scope tp-yt-paper-spinner">
+              <div class="circle style-scope tp-yt-paper-spinner"></div>
+            </div>
+            <div class="circle-clipper right style-scope tp-yt-paper-spinner">
+              <div class="circle style-scope tp-yt-paper-spinner"></div>
+            </div>
+          </div>
+          <div class="spinner-layer layer-3 style-scope tp-yt-paper-spinner">
+            <div class="circle-clipper left style-scope tp-yt-paper-spinner">
+              <div class="circle style-scope tp-yt-paper-spinner"></div>
+            </div>
+            <div class="circle-clipper right style-scope tp-yt-paper-spinner">
+              <div class="circle style-scope tp-yt-paper-spinner"></div>
+            </div>
+          </div>
+          <div class="spinner-layer layer-4 style-scope tp-yt-paper-spinner">
+            <div class="circle-clipper left style-scope tp-yt-paper-spinner">
+              <div class="circle style-scope tp-yt-paper-spinner"></div>
+            </div>
+            <div class="circle-clipper right style-scope tp-yt-paper-spinner">
+              <div class="circle style-scope tp-yt-paper-spinner"></div>
+            </div>
+          </div>
+        </div>
+      </tp-yt-paper-spinner>
     </div>
-    <div class="spinner-layer layer-2 style-scope tp-yt-paper-spinner">
-      <div class="circle-clipper left style-scope tp-yt-paper-spinner">
-        <div class="circle style-scope tp-yt-paper-spinner"></div>
-      </div>
-      <div class="circle-clipper right style-scope tp-yt-paper-spinner">
-        <div class="circle style-scope tp-yt-paper-spinner"></div>
-      </div>
-    </div>
-    <div class="spinner-layer layer-3 style-scope tp-yt-paper-spinner">
-      <div class="circle-clipper left style-scope tp-yt-paper-spinner">
-        <div class="circle style-scope tp-yt-paper-spinner"></div>
-      </div>
-      <div class="circle-clipper right style-scope tp-yt-paper-spinner">
-        <div class="circle style-scope tp-yt-paper-spinner"></div>
-      </div>
-    </div>
-    <div class="spinner-layer layer-4 style-scope tp-yt-paper-spinner">
-      <div class="circle-clipper left style-scope tp-yt-paper-spinner">
-        <div class="circle style-scope tp-yt-paper-spinner"></div>
-      </div>
-      <div class="circle-clipper right style-scope tp-yt-paper-spinner">
-        <div class="circle style-scope tp-yt-paper-spinner"></div>
-      </div>
-    </div>
-  </div>
-  </tp-yt-paper-spinner>
-  </div>
     <yt-icon-button id="close-button" class="style-scope ytd-unified-share-panel-renderer" role="button" aria-label="취소"><button id="button" class="style-scope yt-icon-button" aria-label="취소">
       <yt-icon icon="close" class="style-scope ytd-unified-share-panel-renderer"><span class="yt-icon-shape style-scope yt-icon yt-spec-icon-shape"><div style="width: 100%; height: 100%; display: block; fill: currentcolor;"><svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%;"><path d="m12.71 12 8.15 8.15-.71.71L12 12.71l-8.15 8.15-.71-.71L11.29 12 3.15 3.85l.71-.71L12 11.29l8.15-8.15.71.71L12.71 12z"></path></svg></div></span></yt-icon>
     </button><yt-interaction id="interaction" class="circular style-scope yt-icon-button"><div class="stroke style-scope yt-interaction"></div><div class="fill style-scope yt-interaction"></div></yt-interaction></yt-icon-button>
@@ -307,10 +308,18 @@ function getQuestionHtml() {
       <img class="thumbnail" />
       <div class="title"></div>
       <div class="question-input-container">
-      <input type="text" value="" placeholder="요약해줘">
-      <button class="question-button"><span class="default-text">요청</span><span class="loading-text">요청 중..</span></button>
+        <input type="text" value="" placeholder="요약해줘">
+        <button class="question-button"><span class="default-text">요청</span><span class="loading-text">요청 중..</span></button>
+      </div>
+      <div class="question-suggestions">
+        <span class="title">Suggestions</span>
+        <ul class="suggestions">
+          <li>요약해줘</li>
+          <li>주요 내용 요약해줘</li>
+          <li>주요 내용 요약해줘</li>
+        </ul>
+      </div>
     </div>
-  </div>
   </ytd-unified-share-panel-renderer>
 </div>
 `;
