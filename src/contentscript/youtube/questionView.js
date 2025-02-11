ab7e8ec459bc24a83c7472761226d9baca697d50
Ed Jeon <muikor@gmail.com>
2025-02-11 17:03:28 +0900

    refactor: Improve error handling and question dialog UI in YouTube content script

--- src/contentscript/youtube/questionView.js
+++ src/contentscript/youtube/questionView.js
@@ -24,6 +24,7 @@ export function showQuestionDialog(videoInfo) {
         hideQuestionDialog();
     });
 
+    setQuestionDialogContent(videoInfo);
     showProgressSpinner(containerElement);
 
     // set dialog position in the center of the screen
@@ -36,13 +37,10 @@ export function showQuestionDialog(videoInfo) {
                 if (chrome.runtime.lastError || response.error) {
                     const error = chrome.runtime.lastError || response.error;
                     console.error("getSuggestedQuestions Error:", error);
-                    showToastMessage(
-                        `Suggested Questions Error: ${error.message}`
-                    );
-                    setQuestionDialogContent(videoInfo);
+                    setError(`Failed to load - ${error.message}`);
                 } else {
                     console.debug("suggested questions response:", response);
-                    setQuestionDialogContent(videoInfo, response);
+                    setSuggestedQuestions(response);
                 }
                 hideProgressSpinner(containerElement);
                 repositionDialog();
@@ -51,7 +49,7 @@ export function showQuestionDialog(videoInfo) {
     } catch (error) {
         if (!handleSendMessageError(error)) {
             console.error("sendMessage getSuggestedQuestions Error:", error);
-            showToastMessage(`Unknown Error: ${error.message}`);
+            setError(`Failed to load - ${error.message}`);
         }
         hideProgressSpinner(containerElement);
         repositionDialog();
@@ -66,24 +64,14 @@ function showProgressSpinner(containerElement) {
     );
     paperSpinnerElement.removeAttribute("aria-hidden");
     paperSpinnerElement.setAttribute("active", "");
-
-    containerElement.querySelector("#share-panel-header").style.display =
-        "none";
-    containerElement.querySelector("#contents").style.display = "none";
 }
 
 function hideProgressSpinner(containerElement) {
     const spinnerElement = containerElement.querySelector("#spinner");
     spinnerElement.setAttribute("hidden", "");
-
-    containerElement.querySelector("#share-panel-header").style.display = "";
-    containerElement.querySelector("#contents").style.display = "";
 }
 
-function setQuestionDialogContent(
-    videoInfo,
-    suggestedQuestionsResponse = null
-) {
+function setQuestionDialogContent(videoInfo) {
     const containerElement = document.querySelector(
         `ytd-popup-container #${containerId}`
     );
@@ -99,38 +87,50 @@ function setQuestionDialogContent(
 
     const suggestionsElement = containerElement.querySelector("ul.suggestions");
     suggestionsElement.innerHTML = "";
+}
 
-    if (suggestedQuestionsResponse) {
-        thumbnailElement.setAttribute(
-            "title",
-            suggestedQuestionsResponse.caption
-        );
+function setSuggestedQuestions(response) {
+    const containerElement = document.querySelector(
+        `ytd-popup-container #${containerId}`
+    );
+    const thumbnailElement = containerElement.querySelector("img.thumbnail");
+    const inputElement = containerElement.querySelector("input[type='text']");
+    const suggestionsElement = containerElement.querySelector("ul.suggestions");
 
-        // For convenience, click the image to automatically input the caption
-        // TODO: Improve user intuitive UI
-        thumbnailElement.addEventListener("click", (e) => {
-            const caption = e.target.getAttribute("title");
-            if (caption) {
-                inputElement.value = caption;
-            }
-        });
+    thumbnailElement.setAttribute("title", response.caption);
 
-        const questions = suggestedQuestionsResponse.questions;
-        if (questions) {
-            const questionClickListener = (e) => {
-                const question = e.target.textContent;
-                inputElement.value = question;
-            };
-
-            questions.forEach((question) => {
-                const li = document.createElement("li");
-                li.textContent = question;
-                suggestionsElement.appendChild(li);
-
-                li.addEventListener("click", questionClickListener);
-            });
+    // For convenience, click the image to automatically input the caption
+    // TODO: Improve user intuitive UI
+    thumbnailElement.addEventListener("click", (e) => {
+        const caption = e.target.getAttribute("title");
+        if (caption) {
+            inputElement.value = caption;
         }
+    });
+
+    const questions = response.questions;
+    if (questions) {
+        const questionClickListener = (e) => {
+            const question = e.target.textContent;
+            inputElement.value = question;
+        };
+
+        questions.forEach((question) => {
+            const li = document.createElement("li");
+            li.textContent = question;
+            suggestionsElement.appendChild(li);
+
+            li.addEventListener("click", questionClickListener);
+        });
     }
+}
+
+function setError(message) {
+    const containerElement = document.querySelector(
+        `ytd-popup-container #${containerId}`
+    );
+    const errorElement = containerElement.querySelector("p.error");
+    errorElement.textContent = message;
 }
 
 function insertQuestionDialog() {
@@ -318,44 +318,6 @@ function getQuestionHtml() {
     return `
 <div id="${containerId}" role="dialog" class="style-scope ytd-popup-container ytq-dialog" style="position: fixed;">
   <ytd-unified-share-panel-renderer class="style-scope ytd-popup-container" tabindex="-1" links-only="true" can-post="">
-    <div id="spinner" class="style-scope ytd-unified-share-panel-renderer" hidden="">
-      <tp-yt-paper-spinner class="style-scope ytd-unified-share-panel-renderer" aria-label="loading" aria-hidden="true">
-        <div id="spinnerContainer" class="  style-scope tp-yt-paper-spinner">
-          <div class="spinner-layer layer-1 style-scope tp-yt-paper-spinner">
-            <div class="circle-clipper left style-scope tp-yt-paper-spinner">
-              <div class="circle style-scope tp-yt-paper-spinner"></div>
-            </div>
-            <div class="circle-clipper right style-scope tp-yt-paper-spinner">
-              <div class="circle style-scope tp-yt-paper-spinner"></div>
-            </div>
-          </div>
-          <div class="spinner-layer layer-2 style-scope tp-yt-paper-spinner">
-            <div class="circle-clipper left style-scope tp-yt-paper-spinner">
-              <div class="circle style-scope tp-yt-paper-spinner"></div>
-            </div>
-            <div class="circle-clipper right style-scope tp-yt-paper-spinner">
-              <div class="circle style-scope tp-yt-paper-spinner"></div>
-            </div>
-          </div>
-          <div class="spinner-layer layer-3 style-scope tp-yt-paper-spinner">
-            <div class="circle-clipper left style-scope tp-yt-paper-spinner">
-              <div class="circle style-scope tp-yt-paper-spinner"></div>
-            </div>
-            <div class="circle-clipper right style-scope tp-yt-paper-spinner">
-              <div class="circle style-scope tp-yt-paper-spinner"></div>
-            </div>
-          </div>
-          <div class="spinner-layer layer-4 style-scope tp-yt-paper-spinner">
-            <div class="circle-clipper left style-scope tp-yt-paper-spinner">
-              <div class="circle style-scope tp-yt-paper-spinner"></div>
-            </div>
-            <div class="circle-clipper right style-scope tp-yt-paper-spinner">
-              <div class="circle style-scope tp-yt-paper-spinner"></div>
-            </div>
-          </div>
-        </div>
-      </tp-yt-paper-spinner>
-    </div>
     <yt-icon-button id="close-button" class="style-scope ytd-unified-share-panel-renderer" role="button" aria-label="취소"><button id="button" class="style-scope yt-icon-button" aria-label="취소">
       <yt-icon icon="close" class="style-scope ytd-unified-share-panel-renderer"><span class="yt-icon-shape style-scope yt-icon yt-spec-icon-shape"><div style="width: 100%; height: 100%; display: block; fill: currentcolor;"><svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%;"><path d="m12.71 12 8.15 8.15-.71.71L12 12.71l-8.15 8.15-.71-.71L11.29 12 3.15 3.85l.71-.71L12 11.29l8.15-8.15.71.71L12.71 12z"></path></svg></div></span></yt-icon>
     </button><yt-interaction id="interaction" class="circular style-scope yt-icon-button"><div class="stroke style-scope yt-interaction"></div><div class="fill style-scope yt-interaction"></div></yt-interaction></yt-icon-button>
@@ -375,9 +337,53 @@ function getQuestionHtml() {
       <div class="question-suggestions">
         <span class="title">Suggestions</span>
         <ul class="suggestions"></ul>
+        <p class="error"></p>
+        ${spinnerHtml}
       </div>
     </div>
   </ytd-unified-share-panel-renderer>
 </div>
 `;
 }
+
+const spinnerHtml = `
+<div id="spinner" class="style-scope ytd-unified-share-panel-renderer" style="height: 111px">
+<tp-yt-paper-spinner class="style-scope ytd-unified-share-panel-renderer" aria-label="loading" active=""><div id="spinnerContainer" class="active  style-scope tp-yt-paper-spinner">
+  <div class="spinner-layer layer-1 style-scope tp-yt-paper-spinner">
+    <div class="circle-clipper left style-scope tp-yt-paper-spinner">
+      <div class="circle style-scope tp-yt-paper-spinner"></div>
+    </div>
+    <div class="circle-clipper right style-scope tp-yt-paper-spinner">
+      <div class="circle style-scope tp-yt-paper-spinner"></div>
+    </div>
+  </div>
+
+  <div class="spinner-layer layer-2 style-scope tp-yt-paper-spinner">
+    <div class="circle-clipper left style-scope tp-yt-paper-spinner">
+      <div class="circle style-scope tp-yt-paper-spinner"></div>
+    </div>
+    <div class="circle-clipper right style-scope tp-yt-paper-spinner">
+      <div class="circle style-scope tp-yt-paper-spinner"></div>
+    </div>
+  </div>
+
+  <div class="spinner-layer layer-3 style-scope tp-yt-paper-spinner">
+    <div class="circle-clipper left style-scope tp-yt-paper-spinner">
+      <div class="circle style-scope tp-yt-paper-spinner"></div>
+    </div>
+    <div class="circle-clipper right style-scope tp-yt-paper-spinner">
+      <div class="circle style-scope tp-yt-paper-spinner"></div>
+    </div>
+  </div>
+
+  <div class="spinner-layer layer-4 style-scope tp-yt-paper-spinner">
+    <div class="circle-clipper left style-scope tp-yt-paper-spinner">
+      <div class="circle style-scope tp-yt-paper-spinner"></div>
+    </div>
+    <div class="circle-clipper right style-scope tp-yt-paper-spinner">
+      <div class="circle style-scope tp-yt-paper-spinner"></div>
+    </div>
+  </div>
+</div>
+</tp-yt-paper-spinner>
+</div>`;
