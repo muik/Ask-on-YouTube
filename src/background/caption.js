524c23080a40e718df23ed51b46db7c6f204a244
Ed Jeon <muikor@gmail.com>
2025-03-27 09:39:01 +0900

    feat: add LRU cache for caption

--- src/background/caption.js
+++ src/background/caption.js
@@ -1,18 +1,31 @@
 import { Errors } from "../errors.js";
 import { generateJsonContent } from "./geminiApi.js";
 import { handleError } from "./handlers.js";
+import { LRUCache } from "./lruCache.js";
 import { getApiKeyRequired } from "./settingsLoader.js";
+
+// Cache for storing captions with a capacity of 30 entries
+const captionCache = new LRUCache(30);
 
 export function getCaption(request, sendResponse) {
     const { imageUrl, imageData } = request;
-    if (!imageUrl && !imageData) {
-        console.error("imageUrl or imageData is required");
+    if (!imageUrl) {
+        console.error("imageUrl is required");
         handleError(sendResponse)(Errors.INVALID_REQUEST);
         return;
     }
 
+    if (captionCache.has(imageUrl)) {
+        sendResponse(captionCache.get(imageUrl));
+        return true;
+    }
+
     getApiKeyRequired()
         .then((apiKey) => requestCaption({ imageUrl, imageData, apiKey }))
+        .then((response) => {
+            captionCache.put(imageUrl, response);
+            return response;
+        })
         .then(sendResponse)
         .catch(handleError(sendResponse));
 
@@ -32,7 +45,7 @@ const responseSchema = {
  * Request to extract caption of an image from Gemini
  * @param {Object} request - The request object
  * @param {string} request.imageUrl - The image URL
- * @param {string} request.imageData - The image data
+ * @param {string} [request.imageData] - Optional image data
  * @param {string} request.apiKey - The API key
  * @returns {Promise<Object>} - The caption
  */
